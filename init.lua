28a4cd48566b0a94582c1d4c7f18d597db606d21
n-kev1n <kevin_@tuta.io>
2021-11-26 19:28:14 +0100

    Removed Vim-scripts

--- init.lua
+++ init.lua
@@ -6,9 +6,5 @@ require('barbar')
 require('keymappings')
 require('opts')
 
--- Vim-Script Configs
-
--- vim.cmd 'source ~/.config/nvim/vim-scripts/coc-config.vim'
-
 -- Theme Settings
 vim.g.colors_name = 'palenight'
