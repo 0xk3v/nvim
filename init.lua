bf607dcdf0738fa057b102af6f739a8d76ae9cdd
n-kev1n <kevin_@tuta.io>
2021-12-24 12:18:07 +0100

    Added New Nvim Tree Config file

--- init.lua
+++ init.lua
@@ -1,11 +1,14 @@
+
 -- Importing Configurations
 
 require('plugins')
+require('nvim-config')
 require('nvim-tree')
+require('tree')
 require('barbar')
 require('keymappings')
 require('opts')
 require('term-config')
-
 -- Theme Settings
-vim.g.colors_name = 'palenight'
+vim.g.colors_name = 'tokyodark'
+vim.g.airline_theme = 'google_dark'
